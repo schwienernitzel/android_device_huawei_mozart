d8ed694e7b543fa475b64ad93d037ffc68678334
Thespartann <danilastefan69@yahoo.ro>
2024-07-07 17:53:24 +0000

    mozart: Enable malloc svelte
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,9 @@ TARGET_KERNEL_CONFIG := mozart_defconfig
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
+# Malloc
+MALLOC_SVELTE := true
+
 # Partitions
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
