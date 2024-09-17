d55d1940ba0e06bdea3e4d0533f66a7198d6c294
schwienernitzel <pfelix0803@gmail.com>
2024-09-17 15:26:13 +0000

    libshim: Add initial camera shims
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -32,3 +32,10 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := camera.cpp
+LOCAL_SHARED_LIBRARIES := libcamera_client
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
