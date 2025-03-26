e099edefa761fa428e1eaa4e716ffa9b46c62bda
schwienernitzel <pfelix0803@gmail.com>
2025-03-26 16:55:55 +0100

    mozart: Configure inline Kernel build
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,25 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_EXFAT_DRIVER := exfat
 TARGET_USERIMAGES_USE_EXT4 := true
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00678000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := \
+    mem=3072M \
+    coherent_pool=512K \
+    mmcparts=mmcblk0:p1(vrl),p2(vrl_backup),p6(modemnvm_factory),p9(splash),p10(modemnvm_backup),p11(modemnvm_img),p12(modemnvm_system),p14(3rdmodemnvm),p15(3rdmodemnvmback),p17(modem_om),p20(modemnvm_update),p30(modem),p31(modem_dsp),p32(dfx),p33(3rdmodem) \
+    androidboot.selinux=permissive \
+    ate_enable=true
+
+BOARD_MKBOOTIMG_ARGS := \
+    --kernel_offset 0x00008000 \
+    --ramdisk_offset 0x07588000 \
+    --tags_offset 0xffb88000
+
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_SOURCE := kernel/huawei/mozart
+TARGET_KERNEL_CONFIG := lineage_mozart_defconfig
+
 # Platform
 TARGET_BOARD_PLATFORM := hi3635
 BOARD_VENDOR_PLATFORM := hi3635
